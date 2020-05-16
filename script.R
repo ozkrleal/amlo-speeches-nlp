b39c41c0ce37741378493fa2e28eba14ba4965e8
Oscar Leal - alienware <ozkralm10@hotmail.com>
2020-05-16 21:10:33 +0200

    limpia

--- script.R
+++ script.R
@@ -3,15 +3,12 @@ library(dplyr)
 library(tidytext)
 
 #first counter is 02/14    257  + 60 days = april 15 2020
-
 start <- as.Date("14/02/20","%d/%m/%y")
 end   <- as.Date("15/04/20","%d/%m/%y")
-
 
 getamloscripts <- function(){
   theDate <- start
   iterator <- 257
-  
   list_tibbles <- list()
 
   while (theDate <= end)
@@ -20,7 +17,6 @@ getamloscripts <- function(){
                  format(theDate, "%m/%d"), 
                  '/version-estenografica-de-la-conferencia-de-prensa-matutina-del-presidente-andres-manuel-lopez-obrador-',
                  as.character(iterator), '/'))
-    
     tryCatch(
       script <-
         read_html(my_url) %>%
